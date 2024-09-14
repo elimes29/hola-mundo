15af14c016c12c989e92bd808bebb6d9fe009b98
Elimes Rodrgíuez <elimes29@gmail.com>
2024-09-13 20:09:02 -0400

    modifica main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("¡Hola Mundo!");
+        System.out.println("¡Bienvenido(a) a Screem Match!");
+        System.out.println("Película: Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean IncuidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        double media = (8.2+6.0+9.3)/3;
+        System.out.println(media);
+
+        String sinopsis = """
+                Matrix es una paradoja.
+                La mejor película del fin del milenio.
+                fué lanzada en:
+                """ + fechaDeLanzamiento + """
+                
+                """+
+                """
+                obtuvo una nota promedio de 
+                """ + media;
+        System.out.println(sinopsis);
+
+        int clasificacion = (int) (media);
+        System.out.println(clasificacion);
+
     }
 }
